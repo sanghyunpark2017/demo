b4233b3756427d7defd3752b7a5446d1bda95296
sanghyunpark2017 <nanene@naver.com>
2017-12-04 11:18:49 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/board/mapper/BoardMapper.java
@@ -0,0 +1,40 @@
+package com.example.demo.board.mapper;
+ 
+import java.util.List;
+ 
+import org.springframework.stereotype.Repository;
+ 
+import com.example.demo.board.domain.BoardVO;
+import com.example.demo.board.domain.FileVO;
+ 
+@Repository("com.example.demo.board.mapper.BoardMapper")
+public interface BoardMapper {
+    //게시글 개수  
+    public int boardCount() throws Exception;
+    
+    //게시글 목록  
+    public List<BoardVO> boardList() throws Exception;
+    
+    //게시글 상세
+    public BoardVO boardDetail(int bno) throws Exception;
+    
+    //게시글 작성  
+    public int boardInsert(BoardVO board) throws Exception;
+    
+    //게시글 수정  
+    public int boardUpdate(BoardVO board) throws Exception;
+    
+    //게시글 삭제  
+    public int boardDelete(int bno) throws Exception;
+    
+   // BoardMapper.java
+    public int fileInsert(FileVO file) throws Exception;
+ 
+
+  //파일 상세
+    public FileVO fileDetail(int bno) throws Exception;
+
+
+}
+ 
+
